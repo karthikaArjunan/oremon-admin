ba6c274624b510173011ae0438d9b969bd4d6dbf
karthikaArjunan <71118073+karthikaArjunan@users.noreply.github.com>
2022-05-28 20:29:27 +0530

    Update urlConfig.js

--- src/urlConfig.js
+++ src/urlConfig.js
@@ -1,4 +1,7 @@
-export const api='http://localhost:2000/api';
+const baseUrl = location.hostname=="localhost" ? http://localhost:2000 : https://oremon-mern.herokuapp.com;
+//const baseUrl = "http://localhost:2000";
+
+export const api='${baseUrl}/api';
 export const generatePublicUrl=(fileName)=>{
-    return `http://localhost:2000/public/${fileName}`
+    return `${baseUrl}/public/${fileName}`
 }
